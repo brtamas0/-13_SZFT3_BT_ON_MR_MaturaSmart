30a6c660b10dd0ae02a9d70eaa55533f7f12b12f
72590478549 <diak_255.biro.tamas.attila@11.d.2026.com>
2026-01-16 00:04:40 +0100

    Tantárgy, témakör model backend

--- MaturaSmart/backend/app/Models/Topic.php
+++ MaturaSmart/backend/app/Models/Topic.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Topic extends Model
 {
-    //
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class);
+    }
+
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
 }
