0adfeeca37462620d48c5e1b9978db30981f6a74
72590478549 <diak_255.biro.tamas.attila@11.d.2026.com>
2026-01-15 15:21:14 +0100

    felhasználó létrehozás hozzáadva

--- MaturaSmart/backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ MaturaSmart/backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -6,44 +6,37 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
+        // 1. Felhasználók
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('password')->nullable(); // Nullable a Google login miatt
+            $table->string('full_name');
+            $table->string('google_id')->nullable()->unique();
+            $table->string('avatar_url')->nullable();
+            $table->integer('graduation_year')->nullable();
+            $table->string('role')->default('student'); // 'student', 'admin', később bővíthető
+
+            // Gamifikáció
+            $table->integer('xp')->default(0);
+            $table->integer('level')->default(1);
+            $table->integer('gems')->default(0);
+
+            //Streak
+            $table->timestamp('streak_start')->nullable();
+            $table->integer('current_streak')->default(0);
+            $table->integer('lost_streak')->default(0);
+            $table->timestamp('last_activity')->nullable();
+
             $table->timestamps();
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
     }
 };
